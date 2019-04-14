21c9060d617c450f15efecb3db31f58e6555d3f3
Sumedh Nimkarde <blesssumedh@gmail.com>
2019-04-14 15:42:02 +0530

    :tada: Init the package

---
+++ greatCircleDistance.js
@@ -0,0 +1,34 @@
+"use strict";
+
+const RADIUS_OF_EARTH = 6371e3;
+const PI = Math.PI;
+
+const distance = options => {
+  const { lat1, lng1, lat2, lng2 } = options;
+
+  const dLat = getRadians(lat2) - getRadians(lat1);
+  const dLng = getRadians(lng2) - getRadians(lng1);
+
+  const φ1 = getRadians(lat1);
+  const φ2 = getRadians(lat2);
+  const Δφ = getRadians(lat2 - lat1);
+  const Δλ = getRadians(lng2 - lng1);
+
+  const a =
+    Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+    Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+  const d = RADIUS_OF_EARTH * c;
+
+  // distance in kms.
+  return d / 1000;
+};
+
+const getRadians = coordinate => {
+  return (coordinate * PI) / 180;
+};
+
+module.exports = {
+    distance
+};
